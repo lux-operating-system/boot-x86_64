e4c5d1f38590184cc673188f253f139183276ae5
jewelcodes <omarmelghoul01@gmail.com>
2024-08-20 15:08:50 -0400

    acpi: structure of ACPI RSDP

---
+++ src/include/acpi.h
@@ -0,0 +1,27 @@
+/*
+ * lux - a lightweight unix-like operating system
+ * Omar Elghoul, 2024
+ * 
+ * Boot loader for the x86_64 architecture
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+typedef struct {
+    // revision == 0
+    char signature[8];      // 'RSD PTR '
+    uint8_t checksum;
+    char oem[6];
+    uint8_t revision;
+    uint32_t rsdt;
+
+    // revision >= 2
+    uint32_t length;
+    uint64_t xsdt;
+    uint8_t extendedChecksum;
+    uint8_t reserved[3];
+} __attribute__((packed)) ACPIRSDP;
+
+ACPIRSDP *findACPIRoot();
