db3387dd46951ee28b2736b661da0af4ca140bc5
jewelcodes <omarmelghoul01@gmail.com>
2024-08-20 16:48:07 -0400

    boot: pass highest physical address to kernel

--- src/core/main.c
+++ src/core/main.c
@@ -58,6 +58,7 @@ int main(LXBootInfo *boot) {
     kernelBootInfo.kernelHighestAddress = kernelHighestAddress;
     kernelBootInfo.kernelTotalSize = kernelHighestAddress - 0x200000;
     kernelBootInfo.acpiRSDP = (uint64_t)rsdp;
+    kernelBootInfo.highestPhysicalAddress = highestPhysicalAddress;
     kernelBootInfo.memoryMap = (uint64_t)memoryMap;
     kernelBootInfo.memoryMapSize = memoryMapSize;
 
